31aa866301554bf63059344fe48b483a96bcc9e3
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-26 20:14:00 -0400

    feat: validações de formulário iniciais

--- public/index.js
+++ public/index.js
@@ -1,1 +1,25 @@
-console.log("Olá mundo");
+/* eslint-disable no-undef */
+
+const emailInput = document.querySelector("#floatingInput");
+const senhaInput = document.querySelector("#floatingPassword");
+const rememberMe = document.querySelector("#flexCheckDefault");
+const mensagemErro = document.querySelector("#mensagemErro");
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (emailInput.value === "" || senhaInput.value === "") {
+        mensagemErro.style.display = "block";
+        mensagemErro.textContent = "Preencha todos os campos";
+        return;
+    }
+
+    mensagemErro.style.display = "none";
+    console.log(
+        "Enviado!",
+        emailInput.value,
+        senhaInput.value,
+        rememberMe.checked
+    );
+});
